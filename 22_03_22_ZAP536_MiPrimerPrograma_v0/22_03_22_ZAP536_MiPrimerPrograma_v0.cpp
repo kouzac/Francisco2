28f96abc3bc28b3f7b5e0e7bf0e6063177d11dd1
Damian Torres B <99357782+kouzac@users.noreply.github.com>
2022-05-03 12:13:47 -0500

    jasdasd

---
+++ 22_03_22_ZAP536_MiPrimerPrograma_v0/22_03_22_ZAP536_MiPrimerPrograma_v0.cpp
@@ -0,0 +1,41 @@
+// 22_03_22_ZAP536_MiPrimerPrograma_v0.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// este es mi primer programa :v Francisco Gerardo Robles De Jesús
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Snake what happen? SNAKE!!? SNAKEEEE!\n"<< std::endl;
+    //Declarar variables
+    int mi_primer_entero = 0;
+    int mi_segundo_entero;
+    int resultado_de_la_suma;
+    std::cout << "ingresa un numero: " << std::endl;
+    std::cin >> mi_primer_entero;
+    std::cout << "ingresa un segundo numero: " << std::endl;
+    std::cin >> mi_segundo_entero;
+
+    resultado_de_la_suma = mi_primer_entero + mi_segundo_entero;
+    std::cout << "El resultado de la suma es: " << resultado_de_la_suma << std::endl;
+    
+    float miPrimerFloat = 0;
+
+    std::cout << "ingresa un numero con decimal" << std::endl;
+    std::cin >> miPrimerFloat;
+    std::cout << "Su numero decimal es: " << miPrimerFloat << std::endl;
+
+    char miPrimerChar = 0;
+    std::cout << "ingrese una letra" << std::endl;
+    std::cin >> miPrimerChar;
+    std::cout << "su carcater es: " << miPrimerChar << std::endl;
+
+    double miPrimerDouble = 0;
+    std::cout << "ingrese un numero grande" << std::endl;
+    std::cin >> miPrimerDouble;
+    std::cout << "su numero es: " << miPrimerDouble << std::endl;
+
+    std::string miPrimerstring;
+    std::cout << "ingrese una palabra" << std::endl;
+    std::cin >> miPrimerstring;
+    std::cout << "su palabra es: " << miPrimerstring <<std::endl;
+}
--- 26_04_2022_ZAP536_arrays/26_04_2022_ZAP536_arrays.cpp
+++ 26_04_2022_ZAP536_arrays/26_04_2022_ZAP536_arrays.cpp
@@ -15,20 +15,35 @@ int main()
 
 	for (int aux = 0; aux <= 4; aux++) {
 		
+<<<<<<< Updated upstream
 		cout << "Ingresa el titulo del libro num. "<< aux << endl;
 		cin.ignore();
 		getline(cin,titulos[aux]);
 		cout << "Ingresa el autor del libro num.  " << aux << endl;
 		getline(cin, autores[aux]);
 		cout << "Ingresa el anio de publicacion del libro num. "<< aux << endl;
+=======
+		cout << "Ingresa el titulo del juego de mesa num. "<< aux << endl;
+		cin.ignore();
+		getline(cin,titulos[aux]);
+		cout << "Ingresa el creador del juego num.  " << aux << endl;
+		getline(cin, autores[aux]);
+		cout << "Ingresa el anio de publicacion del juego num. "<< aux << endl;
+>>>>>>> Stashed changes
 		cin >> anios[aux];
 		
 	}
 	
 	for (int aux=0; aux<=4; aux++) {
+<<<<<<< Updated upstream
 		cout << "El titulo del primer libro es: " << titulos[aux] << endl;
 		cout << "El anio de publicacion del primer libro es: " << anios[aux] << endl;
 		cout << "El autor del primer libro es: " << autores[aux] << endl;
+=======
+		cout << "El titulo del primer juego es: " << titulos[aux] << endl;
+		cout << "El anio de publicacion del primer juego es: " << anios[aux] << endl;
+		cout << "El creador del primer juego es: " << autores[aux] << endl;
+>>>>>>> Stashed changes
 	}
 	
 }
