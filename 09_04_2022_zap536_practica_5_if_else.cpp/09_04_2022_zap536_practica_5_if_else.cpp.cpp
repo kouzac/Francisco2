76387310fc8265cac04c42bd8f71163e9b6f30ee
kouzac <99357782+kouzac@users.noreply.github.com>
2022-05-05 00:40:42 -0500

    Update 09_04_2022_zap536_practica_5_if_else.cpp.cpp
    aumento de la calidad del codigo

--- 09_04_2022_zap536_practica_5_if_else.cpp/09_04_2022_zap536_practica_5_if_else.cpp.cpp
+++ 09_04_2022_zap536_practica_5_if_else.cpp/09_04_2022_zap536_practica_5_if_else.cpp.cpp
@@ -17,28 +17,11 @@ int main()
 	string madre = "Olivia";
 	string discapacidad = "ninguna";
 	string nivel_de_estudio = "preparatoria";
-	//confirmacion de los datos
+	
+	//procesamiento de la informacion
+
 	cout << "ingrese el nombre del paciente \n";
 	cin >> nombre;
-	cout << "Ingrese la edad del paciente \n";
-	cin >> edad;
-	cout << "ingrese la enfermedad del paciente \n";
-	cin >> enfermedad;
-	cout << "Ingrese el mes de naciemiento del paciente \n";
-	cin >> mes_de_nacimiento;
-	cout << "ingrese el genero del paciente \n";
-	cin >> genero;
-	cout << "ingrese la altura del paciente \n";
-	cin >> altura;
-	cout << "ingrese el nombre del padre del paciente \n";
-	cin >> padre;
-	cout << "ingrese el nombre de la madre del paciente \n";
-	cin >> madre;
-	cout << "ingrese si el paciente tiene alguna discapacidad \n";
-	cin >> discapacidad;
-	cout << "ingrese el nivel de estudios que tiene el paciente \n";
-	cin >> nivel_de_estudio;
-	//procesamiento de la informacion
 	if (nombre == "Francisco")
 	{
 		cout << "El nombre del paciente es correcto \n";
@@ -46,7 +29,11 @@ int main()
 	else
 	{
 		cout << "El perfil ingresdo no existe \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "Ingrese la edad del paciente \n";
+	cin >> edad;
 	if (edad == 18)
 	{
 		cout << "Edad confimada \n";
@@ -54,7 +41,11 @@ int main()
 	else
 	{
 		cout << "Esta no es la edad correcta del paciente \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "ingrese la enfermedad del paciente \n";
+	cin >> enfermedad;
 	if (enfermedad == "astigmatismo")
 	{
 		cout << "esta es la enfermedad correcta del paciente \n";
@@ -62,7 +53,11 @@ int main()
 	else
 	{
 		cout << "el paciente no posee esa enfermedad \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "Ingrese el mes de naciemiento del paciente \n";
+	cin >> mes_de_nacimiento;
 	if (mes_de_nacimiento == "julio")
 	{
 		cout << "este es el mes correcto \n";
@@ -70,7 +65,11 @@ int main()
 	else
 	{
 		cout << "este no es el mes correcto \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "ingrese el genero del paciente \n";
+	cin >> genero;
 	if (genero == "Masculino")
 	{
 		cout << "ese es el genero correcto!! \n";
@@ -78,7 +77,11 @@ int main()
 	else
 	{
 		cout << "este no es el genero correcto >:( \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "ingrese la altura del paciente \n";
+	cin >> altura;
 	if (altura == 1.92)
 	{
 		cout << "esta es la altura correcta!! \n";
@@ -86,7 +89,11 @@ int main()
 	else
 	{
 		cout << "esta no es la altura correcta \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "ingrese el nombre del padre del paciente \n";
+	cin >> padre;
 	if (padre == "Saul")
 	{
 		cout << "es el nombre correcto \n";
@@ -94,7 +101,11 @@ int main()
 	else
 	{
 		cout << "este no es el nombre del padre \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "ingrese el nombre de la madre del paciente \n";
+	cin >> madre;
 	if (madre == "Olivia")
 	{
 		cout << "es el nombre correcto de la madre \n";
@@ -103,7 +114,11 @@ int main()
 
 	{
 		cout << "este no es el nombre de la madre del paciente \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "ingrese si el paciente tiene alguna discapacidad \n";
+	cin >> discapacidad;
 	if (discapacidad == "ninguna")
 	{
 		cout << "el paciente no cuenta con una discapacidad \n";
@@ -111,13 +126,21 @@ int main()
 	else
 	{
 		cout << "la discapacidad no es la correcta \n";
+		system("PAUSE");
+		return 0;
 	}
+	cout << "ingrese el nivel de estudios que tiene el paciente \n";
+	cin >> nivel_de_estudio;
 	if (nivel_de_estudio == "preparatoria")
 	{
 		cout << "es el nivel correcto \n";
+
+		cout << "Todos los datos son correctos"
 	}
 	else
 	{
 		cout << "este nivel no es correcto \n";
+		system("PAUSE");
+		return 0;
 	}
 }
